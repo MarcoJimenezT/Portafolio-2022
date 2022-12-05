9cdcb2679b5e4b98d7c63ee844bb0e483ec9e1b2
MarcoJim <marcogiovannij@hotmail.com>
2022-12-04 19:06:02 -0600

    Version 0.1 beta

---
+++ Docs/assets/efectoesc.js
@@ -0,0 +1,15 @@
+
+
+const configuracion = {
+    strings: [`<i>Marco Jimenez</i>`],
+    typeSpeed: 45,
+    backSpeed: 45,
+    startDelay: 300,
+    backDelay: 1500,
+    loop: true,
+    smartBackspace: true, 
+    showCursor: true,
+    loopCount: false
+}
+
+var typed = new Typed('.typed', configuracion)
---
+++ Docs/readme.md
@@ -0,0 +1,1 @@
+#Este es mi portafolio 2022#
